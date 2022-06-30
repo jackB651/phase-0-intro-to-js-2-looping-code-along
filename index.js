f6cb581e52e9ca943cfc741cfbc3e1f4fc469968
Jonathon <jwbialas@gmail.com>
2022-06-30 15:24:11 -0500

    commit tree 06e8b7887ab690a90998fbef8f6e30ad1f7d3593

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solutions in this file
+// Code your solutions in this fil
+const gifts = ["teddy bear", "drone", "doll"];
+function wrapGifts(gifts) {
+for (let i = 0; i < gifts.length; i++ ) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+}
+
+return gifts;
+}
+wrapGifts(gifts);
+
+const name = ["Guadalupe", "Ollie", "Aki"];
+const newArray = []; 
+const event = "suprise";
+function writeCards(name, event) {
+    for (let i = 0; i < name.length; i++ ) {
+        var x =`Thank you, ${name[i]}, for the wonderful ${event} gift!`;
+        console.log(x);
+        newArray.push(x);
+    }
+    return newArray;
+}
+writeCards();
+let number = 4;
+function countDown(number) {
+    while (number >= 0){
+    console.log(number);
+    number--; 
+    }
+}
+countDown(number);
